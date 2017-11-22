541ba242909ec649792ef5bff6c78560377a4f98
root <root@raspberrypi>
2017-11-22 15:18:01 +0200

    Done with RPi tasks

---
+++ Scripts/rpi/json/rust/jsonTask/target/release/jsonTask.d
@@ -0,0 +1,1 @@
+/home/pi/GitHub/Rosetta_Code_Research_MSR/Scripts/rpi/json/rust/jsonTask/target/release/jsonTask: /home/pi/GitHub/Rosetta_Code_Research_MSR/Scripts/rpi/json/rust/jsonTask/src/main.rs
