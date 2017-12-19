ea68b0f9bb6c191a873098b8db15d449b0adfd8d
stefanos1316 <stefanos1316@gmail.com>
2017-12-19 10:52:11 +0200

    Adding changes

--- Scripts/newTask/file-input-output/javascript/file-input-output.js
+++ Scripts/newTask/file-input-output/javascript/file-input-output.js
@@ -1,12 +1,13 @@
 
 for ( var i = 0; i < 1000000; i++) {
-fs = require('fs');
-fs.readFile('../input.txt', 'utf8', function (err, data) {
-            if (err) throw err;
+	fs = require('fs');
+//	fs.readFile('../input.txt', 'utf8', function (err, data) {
+    //        if (err) throw err;
             //Do your processing, MD5, send a satellite to the moon, etc.
-            fs.writeFile ('output.txt', data, function(err) {
-                if (err) throw err;
-            });
-	fs.close();
-        });
+  //          fs.writeFile ('output.txt', data, function(err) {
+      //          if (err) throw err;
+          //  });
+	//fs.close();
+       // });
+var contents = fs.readFileSync('../input.txt', 'utf8')
 }
