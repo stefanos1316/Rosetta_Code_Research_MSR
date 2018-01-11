0a157ec802a759ef96bc243780803d6f4f903226
stefanos1316 <stefanos1316@gmail.com>
2018-01-11 12:15:35 +0200

    Changed .gitmodule

--- Scripts/newTask/regular-expressions/perl/regular-expressions.pl
+++ Scripts/newTask/regular-expressions/perl/regular-expressions.pl
@@ -1,8 +1,7 @@
 $string = "this is a matching string";
 
 for( $a=0; $a < 10000000; $a = $a + 1) {
-if ($string =~ /string$/) {
-}
 
-$string =~ s/ string/ istyfied/; # makes "I am a string" into "I am another string"
+if ($string =~ /string$/) {}
+if ($string =~ s/ string/ istyfied/) {}
 }
